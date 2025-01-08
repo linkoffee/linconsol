5fc873788ace20f1848f73b20cf689570b96c4eb
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2025-01-08 15:41:01 +0500

    refactor[LinConsol]: Добавить точку входа приложения

---
+++ src/LinConsol.cs
@@ -0,0 +1,28 @@
+using System;
+using LinConsol.Commands;
+
+namespace LinConsol
+{
+    class LinConsol
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Welcome to {ProgramInfo.Name}!");
+            Console.WriteLine($"Enter `--help` for list of available commands.");
+
+            while (true)
+            {
+                Console.Write("\n> ");
+                string input = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Please enter a command.\nType `--help` for assistance.");
+                    continue;
+                }
+
+                CommandHandler.ProcessCommand(input);
+            }
+        }
+    }
+}
