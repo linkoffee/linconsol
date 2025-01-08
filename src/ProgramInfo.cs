838ef1b33ee96d9fc05277ff260eaa1d5b9aee0d
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2025-01-08 15:41:45 +0500

    refactor[ProgramInfo]: Добавить настройки информации о приложении в отдельный файл

---
+++ src/ProgramInfo.cs
@@ -0,0 +1,8 @@
+namespace LinConsol
+{
+    static class ProgramInfo
+    {
+        public static readonly string Version = "0.0.12\n  Release date: 2025.01.08";
+        public static readonly string Name = "LINCONSOL";
+    }
+}
