12a64cab0d8a9b9e29e5b9ff81bba7eb4e7754b0
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2025-01-18 23:42:03 +0500

    update: Добавить информацию о новой версии(v0.0.13)

--- src/LinConsol.cs
+++ src/LinConsol.cs
@@ -1,4 +1,3 @@
-using System;
 using LinConsol.Commands;
 
 namespace LinConsol
--- src/ProgramInfo.cs
+++ src/ProgramInfo.cs
@@ -2,7 +2,7 @@ namespace LinConsol
 {
     static class ProgramInfo
     {
-        public static readonly string Version = "0.0.12\n  Release date: 2025.01.08";
+        public static readonly string Version = "0.0.13\n  Release date: 2025.01.18";
         public static readonly string Name = "LINCONSOL";
     }
 }
