4652013bfc33ee8f1c47241ddd7b9d0272e7263d
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2025-02-15 17:02:55 +0500

    Добавить новые команды в настройки (processlist, batteryinfo)

--- src/Commands/CommandHandler.cs
+++ src/Commands/CommandHandler.cs
@@ -2,31 +2,39 @@ namespace LinConsol.Commands
 {
     static class CommandHandler
     {
-        static readonly Dictionary<string, Action> CommandHandlers = new()
+        static readonly Dictionary<string, Action<string[]>> CommandHandlers = new()
         {
-            { "--help", Help.Execute },
-            { "--version", Version.Execute },
-            { "--exit", Exit.Execute },
-            { "--clear", Clear.Execute },
-            { "osinfo", OSInfo.Execute },
-            { "ipinfo", () => IPInfo.Execute().Wait() },
-            { "ipinfo --setkey", IPInfo.SetApiKey },
-            { "ipinfo --showkey", IPInfo.ShowApiKey },
-            { "hwinfo", HWInfo.Execute },
-            { "timeinfo", TimeInfo.Execute },
-            { "ping", Ping.Execute }
+            { "--help", args => Help.Execute() },
+            { "--version", args => Version.Execute() },
+            { "--exit", args => Exit.Execute() },
+            { "--clear", args => Clear.Execute() },
+            { "osinfo", args => OSInfo.Execute() },
+            { "ipinfo", args => IPInfo.Execute().Wait() },
+            { "ipinfo --setkey", args => IPInfo.SetApiKey() },
+            { "ipinfo --showkey", args => IPInfo.ShowApiKey() },
+            { "hwinfo", args => HWInfo.Execute() },
+            { "timeinfo", args => TimeInfo.Execute() },
+            { "batteryinfo", args => BatteryInfo.Execute() },
+            { "processlist", ProcessList.Execute },
+            { "ping", args => Ping.Execute() }
         };
 
-        public static void ProcessCommand(string command)
+        public static void ProcessCommand(string input)
         {
-            if (CommandHandlers.TryGetValue(command.ToLower(), out var handler))
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 0) return;
+
+            string command = parts[0].ToLower();
+            string[] args = parts.Skip(1).ToArray();
+
+            if (CommandHandlers.TryGetValue(command, out var handler))
             {
-                handler();
+                handler(args);
             }
             else
             {
-                Console.WriteLine($"Unknown command `{command}`.");
-                Utils.SuggestCommand.SuggestClosestCommand(command, CommandHandlers.Keys);
+                Console.WriteLine($"Unknown command `{input}`.");
+                Utils.SuggestCommand.SuggestClosestCommand(input, CommandHandlers.Keys);
                 Console.WriteLine($"Or enter `--help` for list of available commands.");
             }
         }
