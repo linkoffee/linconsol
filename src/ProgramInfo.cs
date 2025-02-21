26ce88100e5f0e01e03c65fbf7c2c20832c79468
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2025-02-21 14:31:05 +0500

    chore[config]: Обновить версию (-> 0.0.19)

--- src/ProgramInfo.cs
+++ src/ProgramInfo.cs
@@ -2,7 +2,7 @@ namespace LinConsol
 {
     static class ProgramInfo
     {
-        public static readonly string Version = "0.0.18\n  Release date: 2025.02.17";
+        public static readonly string Version = "0.0.19\n  Release date: 2025.02.21";
         public static readonly string Name = "LINCONSOL";
     }
 }
